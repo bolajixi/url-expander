b4d72b7099a53445c10abde0dd1ce6a47c984add
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-16 09:13:50 +0100

    Add loadin text

--- public/min.js
+++ public/min.js
@@ -3,10 +3,13 @@ const exapandBtn = document.querySelector("#expand-btn");
 const result = document.querySelector("#result");
 
 exapandBtn.addEventListener("click", () => {
+	result.innerText = "Loading...";
+
 	fetch(`/expand?shortUrl=${input.value}`)
 		.then((res) => res.text())
 		.then((text) => {
 			result.innerText = text;
+			result.setAttribute("href", text);
 		})
 		.catch((err) => {
 			console.error(err);
