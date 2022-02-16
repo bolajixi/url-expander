2388e42175b6936844db204f583c8b72734724cc
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-16 08:28:39 +0100

    Add query parameter to the link

--- public/min.js
+++ public/min.js
@@ -2,20 +2,8 @@ const input = document.querySelector("#input");
 const exapandBtn = document.querySelector("#expand-btn");
 const result = document.querySelector("#result");
 
-// exapandBtn.addEventListener("click", () => {
-// 	fetch(`/expand?shortUrl=${input.value}`)
-// 		.then((res) => res.text())
-// 		.then((text) => {
-// 			result.innerText = text;
-// 		})
-// 		.catch((err) => {
-// 			console.error(err);
-// 			result.innerText = "Error";
-// 		});
-// });
-
 exapandBtn.addEventListener("click", () => {
-	fetch(`/expand`)
+	fetch(`/expand?shortUrl=${input.value}`)
 		.then((res) => res.text())
 		.then((text) => {
 			result.innerText = text;
