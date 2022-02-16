88badd8d576a8e97ae0444cbda014a4945833c89
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-16 08:29:18 +0100

    Add expanded routes

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/expand", (req, res) => {
+	const shortUrl = req.query.shortUrl;
+	res.send("Hellow world");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
